5ba64e6dda900f6d398bb5a16b404167e1944ea7
Frankline Were <franklinegift@gmail.com>
2024-06-23 17:24:59 +0300

    return and log in functions

--- index.js
+++ index.js
@@ -1,1 +1,84 @@
 // Follow along with the examples here
+function doNothing() { }
+
+function sayHello() {
+    console.log("Hello!");
+}
+
+sayHello();
+
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+}
+
+sayHelloToGuadalupe();
+console.log("\n");
+
+function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+}
+
+sayHelloToLiz();
+console.log("\n");
+
+function sayHelloToSanip() {
+    console.log("Hello, Sanip!");
+}
+
+sayHelloToSanip();
+console.log("\n");
+
+function sayHelloToAnyone(fname) {
+    console.log(`Hello, ${fname}!`);
+}
+
+sayHelloToAnyone("Moraa");
+let arr = ["Gregory", "Anthony"];
+sayHelloToAnyone(arr);
+console.log("\n");
+
+
+function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+}
+
+say("Goodbye", "Julio");
+console.log("\n");
+
+
+function add(x, y) {
+    return x + y;
+}
+
+console.log(add(1, 2));
+console.log("\n");
+
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+}
+
+console.log(say("Hello", "Liz"));
+console.log("\n");
+
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+}
+
+console.log(say("Hello", "Liz"));
+console.log("\n");
+
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log("I was called!");
+}
+
+console.log(say("Howdy", "partner"));
+console.log("\n");
+
+function studentHonours(fname, honours) {
+    return `${fname} got a ${honours}`
+}
+
+console.log(studentHonours("Frankline", "Second Class Upper"));
